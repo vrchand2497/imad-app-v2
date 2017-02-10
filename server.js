25533a81eabc4043f75851e95586ebfb2365f25c
vrchand2497 <v.r.chand97@gmail.com>
2017-02-10 16:57:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,12 +72,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/one', function (req, res) {
-  res.send(create template(one))
+  res.send(createtemplate(one))
 });
 app.get('/two', function (req, res) {
-  res.send(create template(two));
-});app.get('/three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'three.html'));
+  res.send(createtemplate(two));
+});
+app.get('/three', function (req, res) {
+  res.send(createtemplate(three));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
