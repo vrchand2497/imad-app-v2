a1368eb543b00be74597a317b55a615edee423b6
vrchand2497 <v.r.chand97@gmail.com>
2017-02-11 18:23:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 var articles={
-         'article-one':{itle:'article one',
+         'article-one':{title:'article one',
     head:'one',
     date:'feb 8th',
     content:
