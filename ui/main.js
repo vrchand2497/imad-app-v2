22367618339b6d4be2570dfb1c0d8858d32c0380
vrchand2497 <v.r.chand97@gmail.com>
2017-02-12 12:14:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,9 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="new";
 var element=document.getElementById('madi');
-img.onclick=function(){
+img.onclick=function moveLeft(){
+    marginLeft=marginLeft+10;
+    img.tyle.marginLeft=marginLeft+'px'
     var interval=  setInterval(moveLeft,100);
-    img.style.marginLeft='100px';
+    
 };
