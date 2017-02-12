a18a801f40869ce0c88c5d2725ecdfc499c8b389
vrchand2497 <v.r.chand97@gmail.com>
2017-02-12 16:32:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/', function (req, res) {
 var counter=0;
 app.get('/counter', function (req, res) {
     counter=counter+1;
-  res.send(counter.string());
+  res.send(counter.tostring());
 });
 
 app.get('/:articlename', function (req, res) {
