4a4e98b5538469d859aed7c33e6dc19aa2947080
vrchand2497 <v.r.chand97@gmail.com>
2017-02-12 16:54:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,8 @@
-
+var counter=0;
 var button=document.getElementById('counter');
 button.onclick=function ()
 {
   counter=counter+1;
    var span=document.getElemntById('count');
+   span.innerHTML=counter.tostring();
 };
