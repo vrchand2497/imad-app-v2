3585101cf9e56cda26ef532035665ad99da8e90d
vrchand2497 <v.r.chand97@gmail.com>
2017-02-13 20:40:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,23 @@
 
 var button=document.getElementById('counter');
+var counter=0;
+
 button.onclick=function ()
-{var request= new XMLHttpRequest();
-Request.onreadystatechange = function(){
-    if(request.readystate===XMLHttpRequest.DONE){
-    if(request.status===200){
-       var counter= request.responseText;
+{counter=counter+1;
+//var request= new XMLHttpRequest();
+//Request.onreadystatechange = function(){
+    //if(request.readystate===XMLHttpRequest.DONE){
+    //if(request.status===200){
+       //var counter= request.responseText;
        var  span=document.getElementById('count');
-       span.innerHTML=counter.toString();
-    }
+      span.innerHTML=counter.toString();
+    };
         
-    }
-};
-  request.open('GET','http://vrchand2497.imad.hasura-app.io/counter',true);
-  request.send(null);
+    //}
+//};
+ // request.open('GET','http://vrchand2497.imad.hasura-app.io/counter',true);
+ // request.send(null);
   
   
    
-};
+//};
