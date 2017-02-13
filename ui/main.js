cef315907f9947225b33e0cd7180bc81a490d32b
vrchand2497 <v.r.chand97@gmail.com>
2017-02-13 20:16:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ Request.onreadystatechange = function(){
     if(request.status===200){
        var counter= request.responseText;
        var  span=document.getElementById('count');
-       span.innerHTML=counter.tostring();
+       span.innerHTML=counter.toString();
     }}
 };
   request.open('GET','http://vrchand2497.imad.hasura-app.io/counter',true);
