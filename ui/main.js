05b9164580039403e058a0d0ec30a75fcf54d0a1
vrchand2497 <v.r.chand97@gmail.com>
2017-02-13 20:55:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,10 +7,10 @@ button.onclick=function ()
 var request= new XMLHttpRequest();
 Request.onreadystatechange = function(){
     if(request.readystate===XMLHttpRequest.DONE){
-    if(request.status===200){
-       var counter= request.responseText;
-       var  span=document.getElementById('count');
-      span.innerHTML=counter.toString();
+       if(request.status===200){
+        var counter= request.responseText;
+        var  span=document.getElementById('count');
+        span.innerHTML=counter.toString();
     }
         
     }
