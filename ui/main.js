bbf65b2cacbb9909b0cfd959adb22506549fa3a8
vrchand2497 <v.r.chand97@gmail.com>
2017-02-16 17:29:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ request.send(null);
 };
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
-var submit=document.getElementById('submit button');
+var submit=document.getElementById('submit_btn');
 sumbit.onclick=function(){
    var names=['name1','name2','name3','name4'];
    var list='';
