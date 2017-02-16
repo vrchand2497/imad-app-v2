5e215b633e4ec4bd456f030434df3b5d62fe872f
vrchand2497 <v.r.chand97@gmail.com>
2017-02-16 17:31:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ var submit=document.getElementById('submit_btn');
 sumbit.onclick=function(){
    var names=['name1','name2','name3','name4'];
    var list='';
-   for(var i=o;i<names.length;i++){
+   for(var i=0;i<names.length;i++){
        list+='<li>'+names[i]+'</li>';
        
    }
