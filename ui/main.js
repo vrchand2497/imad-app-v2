df32ea8f1710fcbd05a091ebabc16856b0dccc38
vrchand2497 <v.r.chand97@gmail.com>
2017-02-16 17:46:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ request.send(null);
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
-submit.onclick=function(){
+submit.onclick=function (){
    var names=['name1','name2','name3','name4'];
    var list='';
    for(var i=0;i<names.length;i++){
