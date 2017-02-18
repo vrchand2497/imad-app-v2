5abd87527d4bec29da2bc7d55ab9b729dca13cab
vrchand2497 <v.r.chand97@gmail.com>
2017-02-18 12:11:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,13 +28,24 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function (){
+      var request = new XMLHttpRequest();
+
+
+request.onreadystatechange = function ()
+{
+    if(request.readyState === XMLHttpRequest.DONE)
+    {
+        if(request.status === 200)
+        {
+          var names =request.responseText;
+          names=JSON.parse();
+
+        }
+    }
+
+};
+request.open('GET','http://vrchand2497.imad.hasura-app.io/submit-name?name='+name,true);
+request.send(null);
    var names=['name1','name2','name3','name4'];
-   var list='';
-   for(var i=0;i<names.length;i++){
-       list+='<li>'+names[i]+'</li>';
-       
-   }
-   var ul=document.getElementById('namelist');
-   ul.innerHTML=list;
    
 };
