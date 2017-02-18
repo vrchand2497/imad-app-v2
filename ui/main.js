55e4028a3fd6c37620977ac7d4b77af6aa91d1bf
vrchand2497 <v.r.chand97@gmail.com>
2017-02-18 13:45:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,7 +41,7 @@ request.onreadystatechange = function ()
           var list='';
           for (var i=0; i<names.length;i++)
           {
-              list=='<li>'+names[i]+'</li>';
+              list+='<li>'+names[i]+'</li>';
               
           }
           var ul=document.getElementById('namelist');
