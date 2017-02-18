7f95ed3f49df4ec27bdb28b3f3d9eb2fc9f3d5e7
vrchand2497 <v.r.chand97@gmail.com>
2017-02-18 12:56:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,7 +39,7 @@ request.onreadystatechange = function ()
           var names =request.responseText;
           names=JSON.parse(names);
           var list='';
-          for (var i=0;i<names.length;i++)
+          for (var i=0; i<names.length;i++)
           {
               list=='<li>'+names[i]+'</li>';
               
