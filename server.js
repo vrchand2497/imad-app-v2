c5ec7315a5491f8beaef7ff3b95a2d18614957e8
vrchand2497 <v.r.chand97@gmail.com>
2017-02-18 10:56:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,12 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
- 
+var names=[];
+app.get('/submit-name/:name', function (req, res) {
+  var name=req.params.name;
+  names.push(name);
+  res.send(JSON.stringfy(names));
+}); 
 
 
 
