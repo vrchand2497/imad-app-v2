fa175f525f3e53a3e0777f52151dfba79d685553
vrchand2497 <v.r.chand97@gmail.com>
2017-02-28 20:34:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,10 @@ app.get('/submit-name', function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/test-db', function (req, res) {
+  
+});
+
 var counter=0;
 app.get('/counter', function (req, res) {
     counter=counter+1;
