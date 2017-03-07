6b56220ce41f0201b2e23d4d93a6fbe3fcf20f98
vrchand2497 <v.r.chand97@gmail.com>
2017-03-07 17:50:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,11 @@ app.get('/', function (req, res) {
 var pool = new Pool(config);
 
 app.get('/test-db', function (req, res) {
-  
+pool.query('SELECT*FROM test',function(err ,result){
+    if err{
+        res.status(500).send(err.tostring());
+    }
+}))
 });
 
 var counter=0;
